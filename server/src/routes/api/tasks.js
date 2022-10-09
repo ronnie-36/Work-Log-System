f35e4cb45a270cc1b945d2a975a3b6d8a6ca43a9
Rahul Kumar <rahulkrkr123@gmail.com>
2022-10-09 20:17:59 +0530

    Fixed add tasks and implemeted fetch tasks

--- server/src/routes/api/tasks.js
+++ server/src/routes/api/tasks.js
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 import requireJwtAuth from '../../middleware/requireJwtAuth.js';
 import User from '../../models/User.js';
+import { startOfWeek, endOfWeek } from 'date-fns'
 
 const router = Router();
 
 router.post('/add', requireJwtAuth, async (req, res, next) => {
 
     const { description, taskType, startTime, duration } = req.body;
+
+    const curDateTime = new Date();
+    const startDateTime = new Date(startTime);
+
+    if (startDateTime.getTime() > curDateTime.getTime())
+        return res.status(400).json({ message: 'You can not add tasks for future dates.' });
 
     try {
         const newTask = {
@@ -23,6 +30,27 @@ router.post('/add', requireJwtAuth, async (req, res, next) => {
         });
     } catch (err) {
         return next(err);
+    }
+});
+
+router.get('/', requireJwtAuth, async (req, res) => {
+    try {
+        let { date, employeeId } = req.body;
+        if (req.user.role != 'admin') {
+            employeeId = req.user.id;
+        }
+        date = new Date(date);
+        const weekStart = startOfWeek(date, { weekStartsOn: 1 });
+        const weekEnd = endOfWeek(date, { weekStartsOn: 1 });
+        const user = await User.findById(employeeId);
+        const tasks = user.tasks;
+        let filteredTasks = tasks.filter((task) =>
+            new Date(task.startTime).getTime() >= new Date(weekStart).getTime()
+            && new Date(task.startTime).getTime() <= new Date(weekEnd).getTime()
+        );
+        return res.json(filteredTasks);
+    } catch (err) {
+        res.status(500).json({ message: 'Something went wrong.' });
     }
 });
 
