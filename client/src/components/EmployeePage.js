09caef63664b8148b64f2c764e35cc1721c9f4d0
neelanshu2001 <neelanshugarg097@gmail.com>
2022-10-09 22:52:09 +0530

    Chart basic done

--- client/src/components/EmployeePage.js
+++ client/src/components/EmployeePage.js
@@ -1,10 +1,72 @@
-import React, {useEffect, useState} from "react"; 
+import React, {useEffect, useState , PureComponent } from "react"; 
 import AddTaskModal from "./AddTaskModal";
-
+import { BarChart, Bar, Cell, XAxis, YAxis, Pie, PieChart, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+		
+const data01 = [
+	{ name: 'Group A', value: 400 },
+	{ name: 'Group B', value: 300 },
+	{ name: 'Group C', value: 300 },
+	{ name: 'Group D', value: 200 },
+	{ name: 'Group E', value: 278 },
+	{ name: 'Group F', value: 189 },
+  ];
+  
+  const data02 = [
+	{ name: 'Group A', value: 2400 },
+	{ name: 'Group B', value: 4567 },
+	{ name: 'Group C', value: 1398 },
+	{ name: 'Group D', value: 9800 },
+	{ name: 'Group E', value: 3908 },
+	{ name: 'Group F', value: 4800 },
+  ];
 function EmployeePage(){
 	const [showModal, setShowModal]=useState(false);
 	const [listOfTasks, setListOfTasks]=useState([]);
-	
+	const [data,setData]=useState([
+		  {
+			name: 'Page A',
+			uv: 4000,
+			pv: 2400,
+			amt: 2400,
+		  },
+		  {
+			name: 'Page B',
+			uv: 3000,
+			pv: 1398,
+			amt: 2210,
+		  },
+		  {
+			name: 'Page C',
+			uv: 2000,
+			pv: 9800,
+			amt: 2290,
+		  },
+		  {
+			name: 'Page D',
+			uv: 2780,
+			pv: 3908,
+			amt: 2000,
+		  },
+		  {
+			name: 'Page E',
+			uv: 1890,
+			pv: 4800,
+			amt: 2181,
+		  },
+		  {
+			name: 'Page F',
+			uv: 2390,
+			pv: 3800,
+			amt: 2500,
+		  },
+		  {
+			name: 'Page G',
+			uv: 3490,
+			pv: 4300,
+			amt: 2100,
+		  }
+		])
+
 	function updateTaskList(currTask){
 		setListOfTasks([...listOfTasks,currTask]);
 	}
@@ -42,7 +104,45 @@ function EmployeePage(){
 						/>
 					</div>
 					<div>
-						{/* <EmployeeList ListOfEmployees={ListOfEmployees}/> */}
+					
+						<PieChart width={600} height={400}>
+						<Pie
+							dataKey="amt"
+							nameKey={"name"}
+							isAnimationActive={true}
+							data={data}
+							cx="40%"
+							cy="50%"
+							outerRadius={80}
+							fill="#8884d8"
+							label
+						/>
+						<Pie
+							dataKey="uv"
+							nameKey={"name"}
+							isAnimationActive={true}
+							data={data}
+							cx={500} cy={200}
+							outerRadius={80}
+							fill="#8884d8"
+							label
+						/>
+						<Tooltip/>
+						</PieChart>
+						<ResponsiveContainer width="100%" height={400}>
+
+						<BarChart width={600} height={40} data={data}>
+						<XAxis dataKey="name" />
+							<YAxis />
+							<Tooltip />
+							<Legend />
+							<Bar dataKey="uv">
+							{data.map((entry, index) => (
+								<Cell cursor="pointer" fill={ '#8884d8'} key={`cell-${index}`} />
+							))}
+							</Bar>
+						</BarChart>
+						</ResponsiveContainer>
 					</div>
 				</div>
             </div>
